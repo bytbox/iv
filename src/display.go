b4c318b49d92f7d5a48dab91ca023692b70db19d
Scott Lawrence <bytbox@gmail.com>
2010-07-29 03:03:33 -0400

    display.Display definition improved

--- src/display.go
+++ src/display.go
@@ -7,9 +7,15 @@ import (
 // A Display represents a display of a buffer. Each buffer may have multiple
 // displays, although they will all share the same content.
 type Display struct {
-	Buffer *buffer.Buffer
+	buffer     *buffer.Buffer // the buffer
+	topLine    int            // the first visible line
+	leftColumn int            // the leftmost visible column
+	width      int            // the available width
+	height     int            // the available height
+	yPos       int            // the absolute Y-position of the cursor
+	xPos       int            // the absolute X-position of the cursor
 }
 
-func (d Display) Init(b *buffer.Buffer) {
-	d.Buffer = b
+func (d *Display) Init(b *buffer.Buffer) {
+	d.buffer = b
 }
