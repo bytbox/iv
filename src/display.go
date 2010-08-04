cf7693ad94bb61ea052abbb43effe57816ace722
Scott Lawrence <bytbox@gmail.com>
2010-08-04 11:52:26 -0400

    CursorX and CursorY calculations for Display

--- src/display.go
+++ src/display.go
@@ -10,14 +10,30 @@ type Display struct {
 	buffer     *buffer.Buffer // the buffer
 	topLine    int            // the first visible line
 	leftColumn int            // the leftmost visible column
-	width      int            // the available width
-	height     int            // the available height
+	Width      int            // the available width
+	Height     int            // the available height
 	yPos       int            // the absolute Y-position of the cursor
 	xPos       int            // the absolute X-position of the cursor
 }
 
 func (d *Display) Init(b *buffer.Buffer) {
 	d.buffer = b
+}
+
+//
+// Display information
+//
+
+// CursorX returns the zero-indexed x position, relative to the screen, at
+// which the cursor should be displayed.
+func (d *Display) CursorX() int {
+	return d.xPos
+}
+
+// CursorY returns the zero-indexed y position, relative to the screen, at
+// which the cursor should be displayed.
+func (d *Display) CursorY() int {
+	return d.yPos-d.topLine;
 }
 
 
